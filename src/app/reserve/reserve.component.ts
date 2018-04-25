06f08761bfa1fb8ac633d697f86b2822f4c94332
Roy Stobbelaar <roystobbelaar@hotmail.com>
2018-04-25 19:05:44 +0200

    removed email added register thing

--- src/app/reserve/reserve.component.ts
+++ src/app/reserve/reserve.component.ts
@@ -1,18 +1,22 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
-import {AngularFireDatabase, AngularFireList, AngularFireObject} from 'angularfire2/database';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+  AngularFireObject
+} from 'angularfire2/database';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'reserve',
-  template: `    
-    
-    <div #container class="fade-in">
-    
+  template: `
+    <div #container class="fade-in" *ngIf='!done'>
+
     <h2>Reserveren</h2>
-    
+
     <p>
       Hier onder kunt u kaartjes reserveren voor de show. <br>
       Selecteer het aantal kaartjes, en de student voor wie u komt. <br>
-      Een bevestiging krijgt u via de mail. <br>
+      U krijgt een bevestiging te zien wanneer de reservering is gelukt. <br>
       <br>
     </p>
     <div class="group">
@@ -25,9 +29,9 @@ import {AngularFireDatabase, AngularFireList, AngularFireObject} from 'angularfi
 
       <div class="input-group mb-3">
         <div class="input-group-prepend">
-          <span style="width: 83px" class="input-group-text">E-mail</span>
+          <span style="width: 83px" class="input-group-text">Naam</span>
         </div>
-        <input type="text" [(ngModel)]="email" [placeholder]="'example@gmail.com'" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+        <input type="text" [(ngModel)]="email" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
       </div>
 
       <div class="input-group mb-3">
@@ -45,17 +49,40 @@ import {AngularFireDatabase, AngularFireList, AngularFireObject} from 'angularfi
         Alle tickets zijn al gereserveerd!
       </p>
       <div class="input-group mb-3" style="margin-top: 42px;">
-        <button type="button" [disabled]="ticketsAmount < 1" class="btn btn-primary btn-lg btn-block" (click)="updateTickets()">Reserveer</button>
+        <button type="button" [disabled]="ticketsAmount < 1" class="btn btn-primary btn-lg btn-block" (click)="updateTickets()">
+        Reserveer</button>
       </div>
-      
+
     </div>
-    
+    </div>
+
+    <div #container [@enterAnimation] *ngIf='!!done'>
+
+    <h2>Gelukt! </h2>
+    <p>
+    Uw reservering is gelukt,<br/>
+    we wensen u veel plezier bij de show.
+    </p>
+
+    </div>
+
   `,
-  styleUrls: ['./reserve.component.scss']
+  styleUrls: ['./reserve.component.scss'],
+  animations: [
+    trigger('enterAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('200ms', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('200ms', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 
 export class ReserveComponent implements AfterViewInit {
-
   public items: AngularFireObject<any>;
   public emails: AngularFireList<any>;
 
@@ -63,34 +90,36 @@ export class ReserveComponent implements AfterViewInit {
   public ticketsAmount: number;
   public amount: number;
 
+  public done: boolean;
+
   @ViewChild('container') ref: ElementRef;
 
   ngAfterViewInit(): void {
     setTimeout(() => {
       this.ref.nativeElement.classList.add('fade-out');
-    },0);
+      this.done = false;
+    }, 0);
   }
 
   constructor(public db: AngularFireDatabase) {
-
     this.items = this.db.object('/amount');
-    this.items.valueChanges().subscribe((data:number) => {
+    this.items.valueChanges().subscribe((data: number) => {
       this.ticketsAmount = data;
     });
 
     this.emails = this.db.list('/emails');
-    this.emails.valueChanges().subscribe((data: string[]) => {
-    })
+    this.emails.valueChanges().subscribe((data: string[]) => {});
   }
 
   public updateTickets() {
-
-    if (!this.amount) return;
+    if (!this.amount) {
+      return;
+    }
 
     this.ticketsAmount = this.ticketsAmount - this.amount;
     this.items.set(this.ticketsAmount);
 
     this.emails.push(this.email);
+    this.done = true;
   }
-
 }
